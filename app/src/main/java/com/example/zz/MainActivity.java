633d0c2d6b3d4057f16d288bb35fea5f0690eda4
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-02-25 00:09:05 +0300

    added lifeactivity

--- app/src/main/java/com/example/zz/MainActivity.java
+++ app/src/main/java/com/example/zz/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -18,21 +19,81 @@ public class MainActivity extends AppCompatActivity {
     //Button marklum;
     //TextView text;
 
-
-    private Integer counter = 0;
+    public static final String TAG = "StartActivity";
+    private Integer count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");Log.d(TAG, "onCreate");
 
         //marklum = findViewById(R.id.marklum);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        resetUI();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", count);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("count")) {
+            count = savedInstanceState.getInt("count");
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(count.toString());
+        Log.d(TAG, "resetUI");
+    }
+
     @SuppressLint("SetTextI18n")
     public void onClickBtnAddDog(View view) {
-        counter++;
+        count++;
         TextView counterView = (TextView)findViewById(R.id.txt_counter);
-        counterView.setText(counter.toString());
-        if (counter > 100){
+        counterView.setText(count.toString());
+        if (count > 100){
             counterView.getHeight();
             counterView.getWidth();
             counterView.setHeight(200);
